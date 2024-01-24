f7e46584bd955c0678cb3b567eb199acfefe3ad8
Boki <79083667+EX3mKing@users.noreply.github.com>
2024-01-24 21:19:16 +0100

    Gun Aim

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,24 +5,23 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    Rigidbody rb;
     public float speed;
     public float deathTimer;
     
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.AddForce(transform.forward * speed, ForceMode.Impulse);
         Invoke("SelfDestroy", deathTimer);
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Destroy(gameObject);
-    }
-
+    
     private void SelfDestroy()
     {
         Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if(!Physics.Raycast(transform.position, transform.forward, speed * Time.deltaTime)) 
+            transform.position += transform.forward * (speed * Time.deltaTime);
+        else SelfDestroy();
     }
 }
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -5,11 +5,34 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    [SerializeField] private Transform gun;
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform bulletSpawn;
+    [SerializeField] private Camera camera;
+    public float distance = 30f;
+
+    private Vector3 startLocation;
+    private Vector3 startRotation;
+
+    private void Start()
+    {
+        startLocation = transform.position;
+        startRotation = transform.rotation.eulerAngles;
+    }
 
     private void Update()
     {
+        Vector3 targetLocation = Vector3.forward;
+        
+        RaycastHit hit = new RaycastHit();
+        Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, distance);
+        print(hit.collider);
+        
+        if (hit.collider != null) targetLocation = hit.point;
+        else targetLocation = camera.transform.forward * distance;
+        
+        gun.transform.LookAt(targetLocation);
+        
         if (Input.GetMouseButtonDown(0))
         {
             Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
